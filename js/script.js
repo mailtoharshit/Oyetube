d43f89251d4dfa57ba43eb031f91c154d206a2f3
Harshit Pandey <mailtoharshit@gmail.com>
2017-02-18 15:57:14 -0800

    updated

--- js/script.js
+++ js/script.js
@@ -1,161 +1,151 @@
 $(document).ready(function() {
-    hideResultBox();
-    hideVideoBox();
-    parformUnderlineAnimation();
+            hideResultBox();
+            hideVideoBox();
+            parformUnderlineAnimation();
 
-    var firstTime = 0;
+            var firstTime = 0;
 
-    $(".searchForm").keyup(function() {
-        var query = $(".searchBox").val();
-        if (query != "" && firstTime == 0) {
-            firstTime = 1;
-            afterSearchAnim();
-            //youtubeApiCall(query);
-            var data = youtubeApiCall(query);
-            var videoId = data.items[0].id.videoId;
-            onVideoAnim(videoId);
-            console.log('Badri'+videoId);
+            // $(".searchForm").keyup(function() {
+            //     var query = $(".searchBox").val();
+            //     if (query != "" && firstTime == 0) {
+            //         firstTime = 1;
+            //         afterSearchAnim();
+            //         // youtubeApiCall(query);
+            //     } else if (query != "" && firstTime == 1) {
+            //         clearItems();
+            //         // youtubeApiCall(query);
+            //     }
+            //     return false;
+            // });
 
-        } else if (query != "" && firstTime == 1) {
-            clearItems();
-            var data = youtubeApiCall(query);
-            var videoId = data.items[0].id.videoId;
+            //     $(".searchForm").on("submit", function() {
+            //         var query = $(".searchBox").val();
+            //         if (query != "" && firstTime == 0) {
+            //             firstTime = 1;
+            //             afterSearchAnim();
+            //             // youtubeApiCall(query);
+            //         } else if (query != "" && firstTime == 1) {
+            //             clearItems();
+            //             // youtubeApiCall(query);
+            //         }
+            //         return false;
+            //     });
+            // });
 
-            onVideoAnim(videoId);
-        }
-        return false;
-    });
+            // hide result box
+            var hideResultBox = function() {
+                $(".resutlBox").hide();
+            };
 
-    $(".searchForm").on("submit", function() {
-        var query = $(".searchBox").val();
-        if (query != "" && firstTime == 0) {
-            firstTime = 1;
-            afterSearchAnim();
-            youtubeApiCall(query);
-        } else if (query != "" && firstTime == 1) {
-            clearItems();
-            youtubeApiCall(query);
-        }
-        return false;
-    });
-});
+            // hide video box
+            var hideVideoBox = function() {
+                $(".videoBox").hide();
+            }
 
-// hide result box
-var hideResultBox = function() {
-    $(".resutlBox").hide();
-};
+            // show result box
+            var showResultBox = function(data) {
+                $(".resutlBox").show();
+                showBoxes(data);
+            };
 
-// hide video box
-var hideVideoBox = function() {
-    $(".videoBox").hide();
-}
+            var showBoxes = function(data) {
+                for (var i = 0; i < 10; i++) {
+                    var singleData = data.items[i].snippet;
 
-// show result box
-var showResultBox = function(data) {
-    $(".resutlBox").show();
-    showBoxes(data);
-};
+                    console.log(data.items[i]);
+                    var imgBox = "<img src=\"" + singleData.thumbnails.medium.url + "\" class=\"imgThumb\">";
+                    var title = singleData.title;
+                    var channel = singleData.channelTitle;
+                    var timeInterval = 300;
 
-var showBoxes = function(data) {
-    for (var i = 0; i < 3; i++) {
-        var singleData = data.items[i].snippet;
+                    var resStirng = "<div class=\"eachResult animated slideInUp clearfix videoPlay\" id=\"" + i + "\" > " +
+                        "<div class=\"imgBox\">" + imgBox + "</div> " +
+                        "<div class=\"infoBox\"> " +
+                        "<div class=\"infoBoxMiddle\">" +
+                        "<h2 class=\"videoTitle\">" + title + "</h2> " +
+                        "<p class=\"channelName\">" + channel + "</p> " +
+                        "</div> " +
+                        "</div> " +
+                        "</div>";
+                    $(".resutlBox").append(resStirng);
+                }
 
-        // console.log(data.items[i]);
-        var imgBox = "<img src=\"" + singleData.thumbnails.medium.url + "\" class=\"imgThumb\">";
-        var title = singleData.title;
-        var channel = singleData.channelTitle;
-        var timeInterval = 300;
+                // $(".videoPlay").on("click", function() {
+                //     var id = $(this).attr("id");
+                //     var videoId = data.items[id].id.videoId;
 
-        var resStirng = "<div class=\"eachResult animated slideInUp clearfix videoPlay\" id=\"" + i + "\" > " +
-            "<div class=\"imgBox\">" + imgBox + "</div> " +
-            "<div class=\"infoBox\"> " +
-            "<div class=\"infoBoxMiddle\">" +
-            "<h2 class=\"videoTitle\">" + title + "</h2> " +
-            "<p class=\"channelName\">" + channel + "</p> " +
-            "</div> " +
-            "</div> " +
-            "</div>";
-        $(".resutlBox").append(resStirng);
-    }
+                //     onVideoAnim(videoId);
+                // });
+            };
 
-    $(".videoPlay").on("click", function() {
-        var id = $(this).attr("id");
-        var videoId = data.items[id].id.videoId;
+            // perform aftersearch animation
+            var afterSearchAnim = function() {
+                $(".contentBox").addClass("active");
+                $(".logo").addClass("active");
+                $(".searchBoxArea").addClass("active");
+                $(".slideBox").addClass("slideBoxActive");
+                $(".searchForm").addClass("active");
+                $(".searchBox").addClass("active");
+            };
 
-        onVideoAnim(videoId);
-    });
-};
+            // perform underline animation
+            var parformUnderlineAnimation = function() {
+                $(".searchBox").on("focus", function() {
+                    $(".slideBox.trueColor").addClass("active");
+                });
+                $(".searchBox").on("blur", function() {
+                    $(".slideBox.trueColor").removeClass("active");
+                });
+            };
 
-// perform aftersearch animation
-var afterSearchAnim = function() {
-    $(".contentBox").addClass("active");
-    $(".logo").addClass("active");
-    $(".searchBoxArea").addClass("active");
-    $(".slideBox").addClass("slideBoxActive");
-    $(".searchForm").addClass("active");
-    $(".searchBox").addClass("active");
-};
+            // perform animation on video play
+            var onVideoAnim = function(videoId) {
+                $("#videoFrame").attr("src", "http://www.youtube.com/embed/" + videoId);
 
-// perform underline animation
-var parformUnderlineAnimation = function() {
-    $(".searchBox").on("focus", function() {
-        $(".slideBox.trueColor").addClass("active");
-    });
-    $(".searchBox").on("blur", function() {
-        $(".slideBox.trueColor").removeClass("active");
-    });
-};
+                $(".logo").addClass("fadeOut");
+                $(".searchBoxArea").addClass("fadeOut");
+                $(".resutlBox").addClass("fadeOut");
+                $(".contentBox").addClass("contentBoxBig");
 
-// perform animation on video play
-var onVideoAnim = function(videoId) {
-    $("#videoFrame").attr("src", "https://www.youtube.com/embed/" + videoId + '?autoplay=1');
+                setTimeout(function() {
+                    $(".videoBox").show();
+                }, 1000);
 
-    $(".logo").addClass("fadeOut");
-    $(".searchBoxArea").addClass("fadeOut");
-    $(".resutlBox").addClass("fadeOut");
-    $(".contentBox").addClass("contentBoxBig");
+                $(".closeVideoBtn").on("click", function() {
+                    onVideoClose();
+                });
+            };
 
-    setTimeout(function() {
-        $(".videoBox").show();
-    }, 1000);
+            var onVideoClose = function() {
+                $(".videoBox").addClass("fadeOut");
 
-    $(".closeVideoBtn").on("click", function() {
-        onVideoClose();
-    });
-};
+                setTimeout(function() {
+                    $(".videoBox").hide();
+                    $(".videoBox").removeClass("fadeOut");
+                    $(".contentBox").removeClass("contentBoxBig");
+                    $(".logo").removeClass("fadeOut");
+                    $(".searchBoxArea").removeClass("fadeOut");
+                    $(".resutlBox").removeClass("fadeOut");
+                }, 1000);
+            };
 
-var onVideoClose = function() {
-    $(".videoBox").addClass("fadeOut");
+            // youtubeApiCall
+            var youtubeApiCall = function(query) {
+                $.get(
+                    "https://www.googleapis.com/youtube/v3/search", {
+                        part: 'snippet, id',
+                        q: query,
+                        type: 'video',
+                        key: 'AIzaSyAxUOvZf6kS55s8J81YvOFJkf-esz_cUHk',
+                        maxResults: 10
+                    },
+                    function(data) {
+                        showResultBox(data);
+                    }
+                );
+            };
 
-    setTimeout(function() {
-        $(".videoBox").hide();
-        $(".videoBox").removeClass("fadeOut");
-        $(".contentBox").removeClass("contentBoxBig");
-        $(".logo").removeClass("fadeOut");
-        $(".searchBoxArea").removeClass("fadeOut");
-        $(".resutlBox").removeClass("fadeOut");
-    }, 1000);
-};
-
-// youtubeApiCall
-var youtubeApiCall = function(query) {
-    $.get(
-        "https://www.googleapis.com/youtube/v3/search", {
-            part: 'snippet, id',
-            q: query,
-            type: 'video',
-            key: 'AIzaSyAxUOvZf6kS55s8J81YvOFJkf-esz_cUHk',
-            maxResults: 10
-        },
-        function(data) {
-            showResultBox(data);
-        }
-    );
-
-};
-// console.log(youtubeApiCall);
-
-var clearItems = function() {
-    // console.log("cleard");
-    $(".resutlBox").find(".eachResult").fadeOut("slow");
-};
+            var clearItems = function() {
+                console.log("cleard");
+                $(".resutlBox").find(".eachResult").fadeOut("slow");
+            };
