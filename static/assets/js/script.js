88e031e8ae770420aac983f0351f449a877be21b
Harshit Pandey <mailtoharshit@gmail.com>
2017-02-22 09:18:20 -0800

    Finished

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,20 +1,29 @@
+// hide result box
+var hideResultBox = function() {
+    $(".resutlBox").hide();
+};
+
+// hide video box
+var hideVideoBox = function() {
+        $(".videoBox").hide();
+    }
+    // show result box
+var showResultBox = function(data) {
+    $(".resutlBox").show();
+    showBoxes(data);
+};
 $(document).ready(function() {
     hideResultBox();
     hideVideoBox();
-    parformUnderlineAnimation();
-
     var firstTime = 0;
 
     $(".searchForm").keyup(function() {
         var query = $(".searchBox").val();
         if (query != "" && firstTime == 0) {
             firstTime = 1;
-            // deleteItems();
-            afterSearchAnim();
             youtubeApiCall(query);
         } else if (query != "" && firstTime == 1) {
-            //deleteItems();
-            afterSearchAnim();
+
             youtubeApiCall(query);
 
         }
@@ -26,33 +35,16 @@ $(document).ready(function() {
         var query = $(".searchBox").val();
         if (query != "" && firstTime == 0) {
             firstTime = 1;
-            //deleteItems();
-            afterSearchAnim();
+
             youtubeApiCall(query);
         } else if (query != "" && firstTime == 1) {
-            deleteItems();
-            afterSearchAnim();
             youtubeApiCall(query);
         }
         return false;
     });
 });
 
-// hide result box
-var hideResultBox = function() {
-    $(".resutlBox").hide();
-};
 
-// hide video box
-var hideVideoBox = function() {
-    $(".videoBox").hide();
-}
-
-// show result box
-var showResultBox = function(data) {
-    $(".resutlBox").show();
-    showBoxes(data);
-};
 
 var showBoxes = function(data) {
     for (var i = 0; i < 3; i++) {
@@ -80,22 +72,14 @@ var showBoxes = function(data) {
         var id = $(this).attr("id");
         var videoId = data.items[id].id.videoId;
         var videoTitle = data.items[id].id.videoTitle;
-
         onVideoAnim(videoId, videoTitle);
     });
 };
 
-// perform aftersearch animation
-var afterSearchAnim = function() {
-    $(".contentBox").addClass("active");
-    $(".logo").addClass("active");
-    $(".searchBoxArea").addClass("active");
-    $(".slideBox").addClass("slideBoxActive");
-    $(".searchForm").addClass("active");
-    $(".searchBox").addClass("active");
-};
-
-// perform underline animation
+var relatedVideos = function(title) {
+        youtubeApiCall(title);
+    }
+    // perform underline animation
 var parformUnderlineAnimation = function() {
     $(".searchBox").on("focus", function() {
         $(".slideBox.trueColor").addClass("active");
@@ -108,32 +92,9 @@ var parformUnderlineAnimation = function() {
 // perform animation on video play
 var onVideoAnim = function(videoId, videoTitle) {
     $("#videoFrame").attr("src", "https://www.youtube.com/embed/" + videoId + '?autoplay=1');
-    $("#infoBox").html(videoTitle);
-
-    $(".logo").addClass("fadeOut");
-    $(".searchBoxArea").addClass("fadeOut");
-    $(".resutlBox").addClass("fadeOut");
-    $(".contentBox").addClass("contentBoxBig");
-
+    $("#infoBox").text(videoTitle);
     setTimeout(function() {
         $(".videoBox").show();
-    }, 1000);
-
-    $(".closeVideoBtn").on("click", function() {
-        onVideoClose();
-    });
-};
-
-var onVideoClose = function() {
-    $(".videoBox").addClass("fadeOut");
-
-    setTimeout(function() {
-        $(".videoBox").hide();
-        $(".videoBox").removeClass("fadeOut");
-        $(".contentBox").removeClass("contentBoxBig");
-        $(".logo").removeClass("fadeOut");
-        $(".searchBoxArea").removeClass("fadeOut");
-        $(".resutlBox").removeClass("fadeOut");
     }, 1000);
 };
 
@@ -161,9 +122,7 @@ var youtubeApiCall = function(query) {
             showResultBox(data);
             var videoId = data.items[0].id.videoId;
             var videoTitle = data.items[0].id.videoTitle;
-
             onVideoAnim(videoId, videoTitle);
         }
     );
 };
-
